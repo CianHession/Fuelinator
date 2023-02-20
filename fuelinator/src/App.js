0e99f797e94fdcd4ff3f2f6c514fb9f6fffbd874
Cian Hession <cianhession1@gmail.com>
2023-02-20 12:07:26 +0000

    Login/Register System Upload

--- fuelinator/src/App.js
+++ fuelinator/src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Router } from 'react-router-dom';
+import Dashboard from "./Components/Dashboard";
+import Login from "./Components/Login";
+import Navbar from "./Components/Navbar";
+import Register from "./Components/Register";
 
 function App() {
-
-  return (
-    <main className="App">
-      <Register />
-    </main>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/">
+                    <Login />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/dashboard">
+                    <Navbar />
+                    <Dashboard />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
